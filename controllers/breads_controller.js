f8a9857d70c11194b5bb510cba9f5c698458c2ab
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-03-29 18:59:57 -0400

    bread update

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,17 +1,70 @@
 const express = require("express");
 const breads = express.Router();
-
-// SHOW
-breads.get("/:arrayIndex", (req, res) => {
-  res.send(Bread[req.params.arrayIndex]);
-});
-
-// INDEX
+const Bread = require("../models/bread.js");
+//INDEX
 breads.get("/", (req, res) => {
-  res.render("Index", {
+  res.render("index", {
     breads: Bread,
+    title: "Breads List",
   });
   // res.send(Bread)
+});
+
+//NEW
+breads.get("/new", (req, res) => {
+  res.render("new");
+});
+
+//SHOW
+breads.get("/:indexArray", (req, res) => {
+  if (Bread[req.params.indexArray]) {
+    res.render("Show", {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray,
+    });
+  } else {
+    res.render("404");
+  }
+});
+
+//EDIT
+breads.get("/:indexArray/edit", (req, res) => {
+  res.render("edit", {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray,
+  });
+});
+
+//UPDATE
+breads.put("/:indexArray", (req, res) => {
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread[req.params.indexArray] = req.body;
+  res.redirect(`/breads/${req.params.indexArray}`);
+});
+
+// CREATE
+breads.post("/", (req, res) => {
+  if (!req.body.image) {
+    req.body.image =
+      "https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+  }
+  if (req.body.hasGluten === "on") {
+    req.body.hasGluten = true;
+  } else {
+    req.body.hasGluten = false;
+  }
+  Bread.push(req.body);
+  res.redirect("/breads");
+});
+
+//DELETE
+breads.delete("/:indexArray", (req, res) => {
+  Bread.splice(req.params.indexArray, 1);
+  res.status(303).redirect("/breads");
 });
 
 module.exports = breads;
--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-// DEPENDENCIES
 const express = require("express");
 
-// CONFIGURATION
+//configuration
+
 require("dotenv").config();
 const PORT = process.env.PORT;
+// console.log(PORT)
 const app = express();
 
-// MIDDLEWARE
+//DEPENDENCIES
+const methodOverride = require("method-override");
+//MIDDLEWARE
+app.use(methodOverride("_method"));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-// ROUTES
+//routes
+
 app.get("/", (req, res) => {
-  res.send("Welcome to an Awesome App about Breads!");
+  res.send("Welcome to an Awesome App about Breads!!");
 });
 
-// LISTEN
+//breads
+const breadsController = require("./controllers/breads_controllers.js");
+app.use("/breads", breadsController);
+
+//404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
+
+//listen
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
 });
-
-// ROUTES
-app.get("/", (req, res) => {
-  res.send("Welcome to an Awesome App about Breads");
-});
-
-// Breads
-const breadsController = require("./controllers/breads_controller.js");
-app.use("/breads", breadsController);
